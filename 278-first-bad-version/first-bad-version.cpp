8b6bc24c5152411b1f5c88f482f88967939e4b46
frogface539 <158598889+frogface539@users.noreply.github.com>
2024-05-02 12:56:38 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (14.53%) - LeetSync

---
+++ 278-first-bad-version/first-bad-version.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        long long l=0,h=n,ans=0;
+        while(l<=h){
+            long long  mid=(h+l)/2;
+            if(isBadVersion(mid)) {
+                ans=mid;
+                h=mid-1;
+            }
+            else {
+                l=mid+1;
+            }
+        }
+        return ans;
+    }
+};
