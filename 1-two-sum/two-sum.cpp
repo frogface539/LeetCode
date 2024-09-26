016b22cf3340f82373a53b092984ec68bf8ee85b
frogface539 <158598889+frogface539@users.noreply.github.com>
2024-09-26 20:52:58 +0530

    Time: 108 ms (15.40%) | Memory: 12.9 MB (90.20%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int>ans;
-        for (int i=0 ; i<nums.size() - 1 ; i++){
-            for(int j = i+1 ; j<nums.size() ; j++){
-                if(nums[i] + nums[j]== target){
-                    return{i , j};
+        vector<int> ans;
+
+        // Iterate through each element in the array
+        for (int i = 0; i < nums.size(); i++) {
+            // Iterate through the remaining elements starting from the next index
+            for (int j = i + 1; j < nums.size(); j++) {
+                // Check if the sum of the current elements equals the target
+                if (nums[i] + nums[j] == target) {
+                    // If found, add the indices to the answer vector
+                    ans.push_back(i);
+                    ans.push_back(j);
+                    return ans; // Return the solution early
                 }
             }
         }
-        return {};
+
+        // If no solution is found, return an empty vector
+        return ans;
     }
 };
