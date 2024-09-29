803ccc9b84a2310c0bf572ee83da96b4445dbe15
frogface539 <158598889+frogface539@users.noreply.github.com>
2024-09-29 23:31:06 +0530

    Time: 11 ms (92.46%) | Memory: 18.3 MB (69.65%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    class compare{
+        public:
+        bool operator()(ListNode* a , ListNode* b){
+            return a -> val > b -> val;
+        }
+    };
+
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode* , vector<ListNode*> , compare> pq;
+        
+        for(int i = 0 ; i<lists.size() ; i++){
+            ListNode* listHead = lists[i];
+            if(listHead != NULL){
+                pq.push(listHead);
+            }
+        }
+
+        ListNode* head = NULL;
+        ListNode* tail = NULL;
+
+        while(!pq.empty()){
+            ListNode* topNode = pq.top();
+            pq.pop();
+
+            if(head == NULL){
+                head = topNode;
+                tail = topNode;
+
+                if(tail -> next != NULL){
+                    pq.push(tail -> next);
+                }
+            }
+
+            else{
+                tail -> next = topNode;
+                tail = topNode;
+                if(tail -> next != NULL){
+                    pq.push(tail -> next);
+                }
+            }
+        }
+        return head;
+    }
+};
