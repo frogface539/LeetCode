4855985ee088ebde50d56a5659d2d2b8517d4fe4
Lakshay Jain <158598889+frogface539@users.noreply.github.com>
2024-10-20 19:32:11 +0530

    Time: 78 ms (49.24%) | Memory: 9.8 MB (29.19%) - LeetSync

---
+++ 375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int solve(int start , int end , vector<vector<int>>& dp){
+
+        if(start >= end){
+            return 0;
+        }
+
+        if(dp[start][end] != -1){
+            return dp[start][end];
+        }
+
+        //minimum cost
+        int ans = INT_MAX;
+        for(int i = start ; i<end ; i++){
+            ans = min(ans , i+ max(solve(start , i-1 , dp) , solve(i+1 , end , dp)));
+        }
+        dp[start][end] = ans;
+        return dp[start][end];
+    }
+
+    int getMoneyAmount(int n) {
+        vector<vector<int>>dp(n+1 , vector<int>(n+1 , -1));
+        return solve(1 , n , dp);
+    }
+};
