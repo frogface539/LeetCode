6433f81390df46323745085e4c8f77419d81a514
Lakshay Jain <158598889+frogface539@users.noreply.github.com>
2024-10-28 07:22:42 +0530

    Time: 40 ms (87.50%) | Memory: 84.1 MB (30.11%) - LeetSync

---
+++ 354-russian-doll-envelopes/russian-doll-envelopes.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+        sort(envelopes.begin(), envelopes.end(), [](const vector<int>& a, const vector<int>& b) {
+            if(a[0] == b[0]){
+                return a[1] > b[1];
+            }
+            else{
+                return a[0] < b[0];
+            }
+        });
+ 
+        vector<int> heights;
+        for (const auto& envelope : envelopes) {
+            heights.push_back(envelope[1]);
+        }
+       
+        vector<int> lis;
+        for (int h : heights) {
+            auto it = lower_bound(lis.begin(), lis.end(), h);
+            if (it == lis.end()) {
+                lis.push_back(h);  
+            } else {
+                *it = h; 
+            }
+        }
+
+        return lis.size();
+    }
+};
