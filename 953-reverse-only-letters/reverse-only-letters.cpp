8b012d8832294bafc08477b8a4d4a6faf90906fa
Lakshay Jain <158598889+frogface539@users.noreply.github.com>
2024-11-16 18:04:25 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (12.77%) - LeetSync

--- 953-reverse-only-letters/reverse-only-letters.cpp
+++ 953-reverse-only-letters/reverse-only-letters.cpp
@@ -4,19 +4,18 @@ public:
         int i = 0;
         int j = s.length()-1;
         while(i<j){
-            if(isalpha(s[i]) && isalpha(s[j])){
+            if(!isalpha(s[i])){
+                i++;
+            }
+
+            else if(!isalpha(s[j])){
+                j--;
+            }
+            else{
                 swap(s[i] , s[j]);
                 i++;
                 j--;
             }
-            else if(!isalpha(s[i])){
-                i++;
-            }
-
-            else{
-                j--;
-            }
-            
         }
         return s;
     }
