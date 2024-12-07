8a705f9ca91ec1f20d48322f1279ef665912bed9
Lakshay Jain <158598889+frogface539@users.noreply.github.com>
2024-12-08 01:28:56 +0530

    Time: 23 ms (83.54%) | Memory: 61.3 MB (72.82%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        int cnt=0;
         for(int i=0;i<nums.size()-1;i++)
         {
             if(nums[i]==nums[i+1])
