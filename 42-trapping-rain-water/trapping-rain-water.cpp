50dcbee4a56db8f7532663a6b6008b22777fc2d7
Lakshay Jain <158598889+frogface539@users.noreply.github.com>
2025-02-07 01:25:26 +0530

    Time: 0 ms (100.00%) | Memory: 27.4 MB (20.63%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,53 +1,72 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        // vector<int>leftMax(height.size());
-        // vector<int>rightMax(height.size());
+        
+        //array me left max and right max nikalne ke liye calculation
+        vector<int>leftMax(height.size());
+        vector<int>rightMax(height.size());
 
-        // leftMax[0] = height[0];
-        // for(int i=1 ; i<height.size() ; i++){
-        //     leftMax[i] = max(leftMax[i-1] , height[i]);
-        // }
+        leftMax[0] = height[0];
+        for(int i=1 ; i<height.size() ; i++){
+            leftMax[i] = max(leftMax[i-1] , height[i]);
+        }
 
-        // rightMax[height.size()-1] = height[height.size()-1];
-        // for(int i=height.size() - 2; i>=0 ; i--){
-        //     rightMax[i] = max(rightMax[i+1] , height[i]);
-        // }
+        rightMax[height.size()-1] = height[height.size()-1];
+        for(int i=height.size() - 2; i>=0 ; i--){
+            rightMax[i] = max(rightMax[i+1] , height[i]);
+        }
 
-        // int sum = 0;
-        // for(int i=0 ; i<height.size() ; i++){
-        //     int hw = min(leftMax[i] , rightMax[i]) - height[i];
-        //     sum = sum + hw;
-        // }
-        // return sum;
+        // trapped water ki height nikaal ne ke liye calculation 
+        // isme leftmax aur right max ka minimum nikaala jisse minumum water stores nikle and array[i] se         subtract kardiya 
 
-        int left = 0;
-        int right = height.size()-1;
-        int rightMax = 0;
-        int leftMax = 0;
+        // do dry run it !!
+
         int sum = 0;
-
-        while(left <= right){
-            if(height[right] >= height[left]){
-                if(leftMax <= height[left]){
-                    leftMax = height[left];
-                }
-                else{
-                    sum = sum + leftMax - height[left];
-                }
-                left++;
-            }
-            
-            else{
-                if(rightMax <= height[right]){
-                    rightMax = height[right];
-                }
-                else{
-                    sum = sum + rightMax - height[right];
-                }
-                right--;
-            }
+        for(int i=0 ; i<height.size() ; i++){
+            int hw = min(leftMax[i] , rightMax[i]) - height[i];
+            sum = sum + hw;
         }
         return sum;
+
+
+
+
+
+
+
+
+
+
+
+        // //without using extra space 
+        
+        // int left = 0;
+        // int right = height.size()-1;
+        // int rightMax = 0;
+        // int leftMax = 0;
+        // int sum = 0;
+
+        // while(left <= right){
+        //     if(height[right] >= height[left]){
+        //         if(leftMax <= height[left]){
+        //             leftMax = height[left];
+        //         }
+        //         else{
+        //             sum = sum + leftMax - height[left];
+        //         }
+        //         left++;
+        //     }
+            
+        //     else{
+        //         if(rightMax <= height[right]){
+        //             rightMax = height[right];
+        //         }
+        //         else{
+        //             sum = sum + rightMax - height[right];
+        //         }
+        //         right--;
+        //     }
+        // }
+        // return sum;
     }
 };
