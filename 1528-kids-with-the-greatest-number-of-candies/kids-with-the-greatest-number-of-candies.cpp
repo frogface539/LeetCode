9ce471831ba947b3b5f8624701b94f986de015a2
Lakshay Jain <158598889+frogface539@users.noreply.github.com>
2025-02-07 01:21:19 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (59.68%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.cpp
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.cpp
@@ -2,24 +2,24 @@ class Solution {
 public:
     vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
         vector<bool>ans;
-        int maxi = INT_MIN;
+
+        //maximum number of candies calculate karne ke liye poori candies array me
+        int maxi = candies[0];
         for(int i=0 ; i<candies.size() ; i++){
-            if(candies[i] > maxi){
-                maxi= candies[i];
+            if(maxi < candies[i]){
+                maxi = candies[i];
             }
         }
 
+        //agar candies at i ka sum + extra candies bada hua maximum candy se to push true else false
         for(int i=0 ; i<candies.size() ; i++){
-            if((extraCandies + candies[i]) >= maxi){
+            if(candies[i] + extraCandies >=maxi){
                 ans.push_back(true);
             }
-
             else{
                 ans.push_back(false);
             }
-
         }
         return ans;
-
     }
 };
