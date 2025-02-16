385d6e81c96290110786b2192b0f2b33e880b021
Lakshay Jain <158598889+frogface539@users.noreply.github.com>
2025-02-16 19:07:50 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (9.48%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = m;
-        for (int j = 0 ; j<n ; j++){
-            nums1[i] = nums2[j];
+        int i = m; //i pointer ko last element pe bhej diya nums1 ke
+        for(int j=0 ; j<n ; j++){
+            nums1[i] = nums2[j]; //i ke baad nums2 ke elements ko allocate kardiya
             i++;
         }
-        sort(nums1.begin(),nums1.end());
+        sort(nums1.begin() , nums1.end());
     }
 };
