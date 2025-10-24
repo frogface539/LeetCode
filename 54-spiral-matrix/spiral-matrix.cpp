90ed25fff3e165af40535974ec3e0b4265dbaec9
Lakshay Jain <158598889+frogface539@users.noreply.github.com>
2025-10-24 16:15:21 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (78.85%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -5,7 +5,6 @@ public:
         if (matrix.empty() || matrix[0].empty()) {
             return result;
         }
-        
         int rows = matrix.size(), cols = matrix[0].size();
         int left = 0, right = cols-1, top = 0, bottom = rows-1;
         
@@ -14,19 +13,16 @@ public:
                 result.push_back(matrix[top][i]);
             }
             top++;
-            
             for (int i = top; i <= bottom; i++) {
                 result.push_back(matrix[i][right]);
             }
             right--;
-            
             if (top <= bottom) {
                 for (int i = right; i >= left; i--) {
                     result.push_back(matrix[bottom][i]);
                 }
                 bottom--;
-            }
-            
+            }         
             if (left <= right) {
                 for (int i = bottom; i >= top; i--) {
                     result.push_back(matrix[i][left]);
@@ -34,7 +30,6 @@ public:
                 left++;
             }
         }
-        
         return result;
     }
 };
