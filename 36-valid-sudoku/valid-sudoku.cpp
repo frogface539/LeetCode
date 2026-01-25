4f2b48e72bb5d83d968e4d4a5924a8eb1877802e
Lakshay Jain <158598889+frogface539@users.noreply.github.com>
2026-01-25 17:09:07 +0530

    Time: 4 ms (43.34%) | Memory: 24.1 MB (38.69%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    bool validSub(vector<vector<char>>& board, int sr, int er, int sc, int ec) {
+        unordered_set<char> st;
+        for(int row = sr; row<=er; row++) {
+            for(int col = sc; col <= ec; col++) {
+                char ch = board[row][col];
+                if(ch == '.') {
+                    continue;
+                }
+                if(st.count(ch)){
+                    return false;
+                }
+                st.insert(ch);
+            }
+        }
+        return true;
+    }
+    bool isValidSudoku(vector<vector<char>>& board) {
+        for(int i=0 ; i<9 ; i++){
+            unordered_set<int>st;
+            for(int j=0 ; j<9 ; j++){
+                char ch = board[i][j];
+                if(ch == '.'){
+                    continue;
+                }
+                if(st.count(ch)){
+                    return false;
+                }
+                st.insert(ch);
+            }
+        }
+
+        for(int i=0 ; i<9 ; i++){
+            unordered_set<int>st;
+            for(int j=0 ; j<9 ; j++){
+                char ch = board[j][i];
+                if(ch == '.'){
+                    continue;
+                }
+                if(st.count(ch)){
+                    return false;
+                }
+                st.insert(ch);
+            }
+        }
+
+        for(int sr = 0; sr < 9; sr+=3) {
+            int er = sr+2;
+            for(int sc = 0; sc < 9; sc+=3) {
+                int ec = sc+2;
+                if(!validSub(board, sr, er, sc, ec)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
