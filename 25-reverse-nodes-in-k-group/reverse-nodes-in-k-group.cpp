fe4809a4e9828b26c5ba949f135aeb0dcb50abf7
Lakshay Jain <158598889+frogface539@users.noreply.github.com>
2026-02-04 12:42:02 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (38.00%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -10,45 +10,39 @@
  */
 class Solution {
 public:
+    int getLength(ListNode* head){
+        int count = 0;
+        while(head != NULL){
+            count++;
+            head = head->next;
+        }
+        return count;
+    }
+
     ListNode* reverseKGroup(ListNode* head, int k) {
-        // original O(n) TC SC tail recursion soln
-        // take each section out of the list, reverse it, reattach
-        
-        // check if the size of remainder of the unreversed list is at least k 
-        int s = 0; // size of list
-        ListNode* curr = head; 
-        while (curr != NULL) { // run time is O(k) -> O(1)
-            s++;
-            if (s >= k) break;
-            curr = curr->next;
-        } // get the size of the list
-        if (s < k) { // if the remainder size is smaller than k, return the remainder
+        if(head == NULL || head -> next == NULL){
             return head;
         }
-        
-        ListNode* h = head;
-        curr = head; // reuse to save space
-        for (int i = 0; i < k - 1; ++i) { // run time is O(k) -> O(1)
-            curr = curr->next;
-        } // curr->next points at the new head of not yet reversed section
-        // curr points at last item in k items to be reversed
-    
-        ListNode* nh = curr->next; // new head for next sections
-        curr->next = NULL;
-        // before reversal: h = head, curr = end of k items
-        // reverse linked list of size k
-        ListNode* p = NULL;
-        ListNode* c = h;
-        ListNode* n = NULL;
-        while(c != NULL) { // run time is O(k) -> O(1)
-            n = c->next;
-            c->next = p;
-            p = c;
-            c = n;
+
+        int len = getLength(head);
+        if(len < k){
+            return head;
         }
-        // after reversal: curr is reversed head, h = end of k reversed items
-        h->next = reverseKGroup(nh, k); // h is now the pointer to the last elt in the rev list
-        // recurses n/k times, each time runs O(3k) -> O(n)
-        return curr;
+
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        int pos = 0;
+
+        while(pos<k){
+            ListNode* fwd = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = fwd;
+            pos++;
+        }
+
+        ListNode* recHead = reverseKGroup(curr, k);
+        head -> next = recHead;
+        return prev;
     }
 };
