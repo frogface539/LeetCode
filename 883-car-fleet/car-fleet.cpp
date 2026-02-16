1fae4ab154bdffd73e190cdf88ad9e31a70b035b
Lakshay Jain <158598889+frogface539@users.noreply.github.com>
2026-02-16 21:37:31 +0530

    Time: 45 ms (57.46%) | Memory: 105.5 MB (69.51%) - LeetSync

--- 883-car-fleet/car-fleet.cpp
+++ 883-car-fleet/car-fleet.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
-        vector<pair<int , double> >v;
-        int n =position.size();
-        for(int i=0;i< n ;i++)
-        {
-            double t= double(target-position[i])/(double)speed[i];
-           
-            v.push_back({position[i],t});
+        vector<pair<int,int>> cars;
+        for(int i=0 ; i<position.size() ; i++){
+            cars.push_back({position[i],speed[i]});
         }
-        sort(v.begin(),v.end());
 
-        int ans=0;
-        double mxTime=0;
-        for(int i=n-1;i>=0;i--)
-        {
-            if(v[i].second>mxTime)
-            {
-                mxTime=v[i].second;
-                ans++;
+        sort(cars.begin(),cars.end());
+        stack<double>st;
+
+        for(int i=0 ; i<cars.size() ; i++){
+            double time = (double)(target-cars[i].first)/cars[i].second; // time = distance/speed
+
+            while(!st.empty() && time >= st.top()){
+                st.pop();
             }
+            st.push(time);
         }
-        return ans;
+        return st.size();
     }
 };
