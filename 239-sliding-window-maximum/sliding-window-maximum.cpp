a0be9da2fff4086e1c4069f291e38e0e805c4b57
Lakshay Jain <158598889+frogface539@users.noreply.github.com>
2026-02-22 21:26:24 +0530

    Time: 23 ms (59.69%) | Memory: 139.3 MB (43.64%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
-    std::vector<int> maxSlidingWindow(const std::vector<int>& nums, int k) {
-    std::vector<int> max_in_windows;
-    std::deque<int> dq; // Store indices of potential maximum elements
-    
-    for (int i = 0; i < nums.size(); ++i) {
-        // Remove indices that are out of the current window
-        if (!dq.empty() && dq.front() < i - k + 1) {
-            dq.pop_front();
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int>q;
+        vector<int>ans;
+
+        //process one window
+        for(int i=0 ; i<k ; i++){
+            int element = nums[i];
+            while(!q.empty() && nums[q.back()] < element){
+                q.pop_back();
+            }
+            q.push_back(i);
         }
-        
-        // Remove elements from the back of the deque while they are less than or equal to the current element
-        while (!dq.empty() && nums[dq.back()] <= nums[i]) {
-            dq.pop_back();
+        ans.push_back(nums[q.front()]);
+
+        //process remaining windows
+        for(int i=k ; i<nums.size() ; i++){
+
+            //remove out of window
+            if(q.front() < i-k+1){
+                q.pop_front();
+            }
+
+            // add logic
+            int element = nums[i];
+            while(!q.empty() && nums[q.back()] < element){
+                q.pop_back();
+            }
+            q.push_back(i);
+            ans.push_back(nums[q.front()]);
         }
-        
-        // Add the current element's index to the deque
-        dq.push_back(i);
-        
-        // The maximum value for the current window is at the front of the deque
-        if (i >= k - 1) {
-            max_in_windows.push_back(nums[dq.front()]);
-        }
+        return ans;
     }
-    
-    return max_in_windows;
-}
 };
