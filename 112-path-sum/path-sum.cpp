afea3cfcfd650f81101baaf7fabdabad4b93d029
Lakshay Jain <158598889+frogface539@users.noreply.github.com>
2026-03-02 19:12:53 +0530

    Time: 0 ms (100.00%) | Memory: 21.4 MB (94.51%) - LeetSync

--- 112-path-sum/path-sum.cpp
+++ 112-path-sum/path-sum.cpp
@@ -11,26 +11,29 @@
  */
 class Solution {
 public:
-    bool solve(TreeNode* root, int targetSum , int sum){
+    bool solve(TreeNode* root, int targetSum, int sum){
         if(root == NULL){
             return false;
         }
 
-        sum = sum + root -> val;
-        if(root -> left == NULL && root -> right == NULL){
+        sum += root->val;
+        if(root->left == NULL && root->right == NULL){
             if(sum == targetSum){
                 return true;
             }
-            return false;
+            else{
+                return false;
+            }
         }
 
-        bool leftAns = solve(root -> left , targetSum , sum);
-        bool rightAns = solve(root -> right , targetSum , sum);
+        bool leftAns = solve(root->left, targetSum, sum);
+        bool rightAns = solve(root->right, targetSum, sum);
+
         return leftAns || rightAns;
     }
 
     bool hasPathSum(TreeNode* root, int targetSum) {
-        int sum = 0;
-        return solve(root , targetSum , sum);
+        int sum=0;
+        return solve(root,targetSum,sum);
     }
 };
