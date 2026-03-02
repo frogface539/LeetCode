739d01434ade8c113c075dc577d4d63614507b2c
Lakshay Jain <158598889+frogface539@users.noreply.github.com>
2026-03-02 15:30:14 +0530

    Time: 8 ms (90.48%) | Memory: 17.4 MB (38.08%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,33 +9,36 @@
  */
 class Solution {
 public:
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q){
-    if(root == NULL){
-        return NULL;
-    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == NULL){
+            return NULL;
+        }
 
-    if(root == p){
-        return p;
-    }
+        if(p->val == root->val){
+            return p;
+        }
 
-    if(root == q){
-        return q;
-    }
+        if(q->val == root->val){
+            return q;
+        }
 
-    TreeNode* leftAns = lowestCommonAncestor(root -> left , p , q);
-    TreeNode* rightAns = lowestCommonAncestor(root ->right , p , q);
+        TreeNode* leftAns = lowestCommonAncestor(root->left, p,q);
+        TreeNode* rightAns = lowestCommonAncestor(root->right, p,q);
 
-    if(leftAns != NULL && rightAns == NULL){
-        return leftAns;
-    }
+        if(leftAns == NULL && rightAns == NULL){
+            return NULL;
+        }
 
-    if(leftAns == NULL && rightAns != NULL){
-        return rightAns;
-    }
+        else if(leftAns == NULL && rightAns != NULL){
+            return rightAns;
+        }
 
-    if(leftAns == NULL && rightAns == NULL){
-        return NULL;
+        else if(leftAns != NULL && rightAns == NULL){
+            return leftAns;
+        }
+
+        else{
+            return root;
+        }
     }
-    return root;
-}
 };
