53f554a7022b9afc7517e4d0bd3f1db1cbd31464
Lakshay Jain <158598889+frogface539@users.noreply.github.com>
2026-03-16 15:05:51 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (77.11%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -12,38 +12,41 @@
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-    vector<vector<int>> res;
-    if(!root) return res;
-    deque<TreeNode*> dq;
-    int level=0;
-    dq.push_back(root);
-    while(!dq.empty()){
-        int l=dq.size();
-        vector<int> temp;
-        for(int i=0;i<l;i++){
-            if(level%2==0){
-                TreeNode* node= dq.back();
-                dq.pop_back();
-                temp.push_back(node->val);
-                if(node->left) dq.push_front(node->left);
-                if(node->right) dq.push_front(node->right);
-
-            }
-            else{
-                TreeNode* node= dq.front();
-                dq.pop_front();
-                temp.push_back(node->val);
-                if(node->right) dq.push_back(node->right);
-                if(node->left) dq.push_back(node->left);
-
-            }
+        vector<vector<int>>output;
+        if(root == NULL){
+            return output;
         }
-        level++;
-        res.push_back(temp);
-    }
+        queue<TreeNode*>q;
+        q.push(root);
+        bool flag = true;
 
-    
+        while(!q.empty()){
+            int size = q.size();
+            vector<int>ans(size);
 
-    return res;
+            for(int i=0 ; i<size ; i++){
+                TreeNode* front = q.front();
+                q.pop();
+
+                int index;
+                if(flag){
+                    index = i;
+                }
+                else{
+                    index = size-i-1;
+                }
+                ans[index] = front->val;
+
+                if(front -> left){
+                    q.push(front->left);
+                }
+                if(front -> right){
+                    q.push(front->right);
+                }
+            }
+            flag = !flag;
+            output.push_back(ans);
+        }
+        return output;
     }
 };
