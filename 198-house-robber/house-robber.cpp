071798f5ec9e2f0b5738d1e58f2bec8b8efe4d27
Lakshay Jain <158598889+frogface539@users.noreply.github.com>
2026-04-03 16:38:52 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (60.37%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,23 +1,33 @@
 class Solution {
 public:
-    int solveMem(vector<int>& nums , int index , vector<int>& dp){
+    int solve(vector<int>& nums, int index){
         if(index >= nums.size()){
             return 0;
         }
-        if(dp[index] != -1){
-            return dp[index];
+
+        int include = nums[index] + solve(nums, index + 2);
+        int exclude = solve(nums,index+1);
+
+        return max(include,exclude);
+    }
+
+    int solveMem(vector<int>& arr, vector<int>& dp, int i){
+        if(i >= arr.size()){
+            return 0;
         }
 
-        int include = nums[index] + solveMem(nums , index + 2 , dp);
-        int exclude = 0 + solveMem(nums , index + 1 , dp);
-        //store ans in dp array
-        dp[index] = max(include , exclude);
-        return dp[index];
+        if(dp[i] != -1){
+            return dp[i];
+        }
+
+        int include = arr[i] + solveMem(arr,dp,i+2);
+        int exculde = solveMem(arr,dp,i+1);
+        dp[i] = max(include,exculde);
+        return dp[i];
     }
 
     int rob(vector<int>& nums) {
-        vector<int>dp(nums.size()+1 , -1);
-        int index = 0;
-        return solveMem(nums , index , dp);
+        vector<int>dp(nums.size()+1,-1);
+        return solveMem(nums, dp, 0);
     }
 };
