6acc57b7664059fe12efe268d4345e5f47e72fa0
Lakshay Jain <158598889+frogface539@users.noreply.github.com>
2026-04-04 19:01:20 +0530

    Time: 468 ms (21.19%) | Memory: 295.6 MB (25.01%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,28 +1,42 @@
 class Solution {
 public:
-    int solve(vector<int>& nums, int index, int lastIndex, vector<vector<int>>& dp) {
-        if (index >= nums.size()) {
+    int solve(vector<int>&arr, int last, int i){
+        if(i>=arr.size()){
+            return 0;
+        }
+        int include = 0;
+        if(arr[i]>last){
+            include = 1+solve(arr,arr[i],i+1);
+        }
+        int exclude = 0+solve(arr,last,i+1);
+        return max(include,exclude);
+    }
+
+    int solveMem(vector<int>& arr, int lastIndex, int i, vector<vector<int>>& dp) {
+        if (i >= arr.size()) {
             return 0;
         }
 
-        if (dp[lastIndex + 1][index] != -1) {
-            return dp[lastIndex + 1][index];
+        if (dp[lastIndex + 1][i] != -1) {
+            return dp[lastIndex + 1][i];
         }
 
         int include = 0;
-        int curr = nums[index];
-        if (lastIndex == -1 || curr > nums[lastIndex]) {
-            include = 1 + solve(nums, index + 1, index, dp);
-        }
-        int exclude = solve(nums, index + 1, lastIndex, dp);
 
-        dp[lastIndex + 1][index] = max(include, exclude);
-        return dp[lastIndex + 1][index];
+        if (lastIndex == -1 || arr[lastIndex] < arr[i]) {
+            include = 1 + solveMem(arr, i, i + 1, dp);
+        }
+
+        int exclude = solveMem(arr, lastIndex, i + 1, dp);
+
+        return dp[lastIndex + 1][i] = max(include, exclude);
     }
 
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
+
         vector<vector<int>> dp(n + 1, vector<int>(n, -1));
-        return solve(nums, 0, -1, dp);
+
+        return solveMem(nums, -1, 0, dp);
     }
 };
