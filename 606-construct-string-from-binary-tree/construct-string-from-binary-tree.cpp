957c2fb621e8d1fdaf903317191666c4457b15c3
Lakshay Jain <158598889+frogface539@users.noreply.github.com>
2026-04-05 21:07:05 +0530

    Time: 19 ms (47.66%) | Memory: 56.2 MB (52.70%) - LeetSync

--- 606-construct-string-from-binary-tree/construct-string-from-binary-tree.cpp
+++ 606-construct-string-from-binary-tree/construct-string-from-binary-tree.cpp
@@ -12,26 +12,16 @@
 class Solution {
 public:
     string tree2str(TreeNode* root) {
-        string str = "";
-         check(root, str);
-         return str;
-    }
-    void check(TreeNode* root, string &str) {
-        if (root == NULL) {
-            return;
-        }
-        str += to_string(root->val);
-        if (root->left || root->right) {
-            str += '(';
-            check(root->left, str);
-            str += ')';
-        }
-        if (root->right) {
-            str += '(';
-            check(root->right, str);
-            str += ')';
-        }
+		string ans = to_string(root->val);
+
+		if (root->left)
+			ans += "(" + tree2str(root->left) + ")";
+
+		if (root->right) { 
+			if (!root->left) ans += "()";
+			ans += "(" + tree2str(root->right) + ")"; 
+		}
         
-    }
-    
+		return ans;
+	}
 };
