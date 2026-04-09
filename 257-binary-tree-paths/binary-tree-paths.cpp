10dd5674d5780151cee02d232b304330a6b849b3
Lakshay Jain <158598889+frogface539@users.noreply.github.com>
2026-04-09 21:43:52 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (23.63%) - LeetSync

---
+++ 257-binary-tree-paths/binary-tree-paths.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    public:
+
+    void dfs(TreeNode* node, string path, vector<string>& result) {
+        if (!node) return;
+        path += to_string(node->val);
+        if (!node->left && !node->right) {
+            result.push_back(path);
+        } 
+        else {
+            dfs(node->left, path + "->", result);
+            dfs(node->right, path + "->", result);
+        }
+    }
+
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string> result;
+        dfs(root, "", result);
+        return result;
+    }
+};
