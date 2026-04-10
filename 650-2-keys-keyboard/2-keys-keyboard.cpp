4387572a271202ceb46f23e853be4525528dbed3
Lakshay Jain <158598889+frogface539@users.noreply.github.com>
2026-04-10 21:19:52 +0530

    Time: 73 ms (7.88%) | Memory: 159 MB (10.60%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int solve(int n, int currA, int clipA){
+    int solve(int n, int currA, int clipA, vector<vector<int>>& dp){
         if(currA == n){
             return 0;
         }
@@ -9,16 +9,21 @@ public:
             return 1e9;
         }
 
-        int copyPasteAll = 1 + 1 + solve(n,currA+currA,currA);
-        int paste = 1+ solve(n,currA+clipA, clipA);
+        if(dp[currA][clipA] != -1){
+            return dp[currA][clipA];
+        }
 
-        return min(copyPasteAll,paste);
+        int copyPasteAll = 1 + 1 + solve(n,currA+currA,currA,dp);
+        int paste = 1+ solve(n,currA+clipA, clipA,dp);
+
+        return dp[currA][clipA] = min(copyPasteAll,paste);
     }
 
     int minSteps(int n) {
         if(n==1){
             return 0;
         }
-        return 1+solve(n,1,1);
+        vector<vector<int>>dp(n+1, vector<int>(n+1,-1));
+        return 1+solve(n,1,1,dp);
     }
 };
